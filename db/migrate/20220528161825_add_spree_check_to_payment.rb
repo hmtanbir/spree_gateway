4720e3c6096894983ea75be644c6e897c3d5f78f
HM Tanbir <tanbir2025@gmail.com>
2022-05-29 11:43:57 +0600

    add check number in payment

--- db/migrate/20220528161825_add_spree_check_to_payment.rb
+++ db/migrate/20220528161825_add_spree_check_to_payment.rb
@@ -1,6 +1,7 @@
 class AddSpreeCheckToPayment < ActiveRecord::Migration[5.2]
   def change
-    add_column :spree_payments, :account_number, :string, null: true
     add_column :spree_payments, :routing_number, :string, null: true
+    add_column :spree_payments, :account_number, :string, null: true
+    add_column :spree_payments, :check_number, :string, null: true    
   end
 end
