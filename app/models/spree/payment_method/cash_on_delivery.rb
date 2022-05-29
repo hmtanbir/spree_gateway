2989df676cd6f58db9fd2e3e5b5a72328b963f59
HM Tanbir <tanbir2025@gmail.com>
2022-05-29 11:58:28 +0600

    Fix indentation

--- app/models/spree/payment_method/cash_on_delivery.rb
+++ app/models/spree/payment_method/cash_on_delivery.rb
@@ -1,44 +1,43 @@
 module Spree
-    class PaymentMethod::CashOnDelivery < ::Spree::PaymentMethod
-      def actions
-        %w{capture void}
-      end
+  class PaymentMethod::CashOnDelivery < ::Spree::PaymentMethod
+    def actions
+      %w{capture void}
+    end
   
-      # Indicates whether its possible to capture the payment
-      def can_capture?(payment)
-        ['checkout', 'pending'].include?(payment.state)
-      end
+    # Indicates whether its possible to capture the payment
+    def can_capture?(payment)
+      ['checkout', 'pending'].include?(payment.state)
+    end
   
-      # Indicates whether its possible to void the payment.
-      def can_void?(payment)
-        payment.state != 'void'
-      end
+    # Indicates whether its possible to void the payment.
+    def can_void?(payment)
+      payment.state != 'void'
+    end
   
-      def capture(*)
-        simulated_successful_billing_response
-      end
+    def capture(*)
+      simulated_successful_billing_response
+    end
   
-      def cancel(*)
-        simulated_successful_billing_response
-      end
+    def cancel(*)
+      simulated_successful_billing_response
+    end
   
-      def void(*)
-        simulated_successful_billing_response
-      end
+    def void(*)
+      simulated_successful_billing_response
+    end
   
-      def source_required?
-        false
-      end
+    def source_required?
+      false
+    end
   
-      def credit(*)
-        simulated_successful_billing_response
-      end
+    def credit(*)
+      simulated_successful_billing_response
+    end
   
-      private
+    private
   
-      def simulated_successful_billing_response
-        ActiveMerchant::Billing::Response.new(true, '', {}, {})
-      end
+    def simulated_successful_billing_response
+      ActiveMerchant::Billing::Response.new(true, '', {}, {})
     end
   end
-  
+end
